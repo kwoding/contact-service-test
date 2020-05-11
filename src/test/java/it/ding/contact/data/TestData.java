35bc6062ab933f54c81e2a87d343a099432a2b51
Kwo Ding <kwo@ding.it>
2020-05-11 17:24:17 +0200

    Set up exercise 5

--- src/test/java/it/ding/contact/data/TestData.java
+++ src/test/java/it/ding/contact/data/TestData.java
@@ -2,6 +2,9 @@ package it.ding.contact.data;
 
 public class TestData {
 
+    private TestData() {
+    }
+
     public static final String ADMIN = "admin";
 
     public static final String ADMIN_PASSWORD = "admin";
