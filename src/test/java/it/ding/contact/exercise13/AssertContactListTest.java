2f1c0b78de9a79823a8f29325ec64eda616be590
Kwo Ding <kwo@ding.it>
2020-05-11 21:12:51 +0200

    Update exercise 13

--- src/test/java/it/ding/contact/exercise13/AssertContactListTest.java
+++ src/test/java/it/ding/contact/exercise13/AssertContactListTest.java
@@ -4,14 +4,16 @@ import static it.ding.contact.data.TestData.ADMIN;
 import static it.ding.contact.data.TestData.ADMIN_PASSWORD;
 import static it.ding.contact.util.ContactTestUtil.generateContactWithAllFieldsFilled;
 import static org.apache.http.HttpStatus.SC_CREATED;
+import static org.apache.http.HttpStatus.SC_OK;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import it.ding.contact.client.ContactRestClientV6;
 import it.ding.contact.client.ContactRestClientV7;
 import it.ding.contact.client.LoginRestClient;
 import it.ding.contact.model.Contact;
 import it.ding.contact.model.ContactListGetResponseBody;
+import it.ding.contact.model.ContactPostResponseBody;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,24 +32,38 @@ public class AssertContactListTest {
 
     @Test
     void canAssertContactList() {
-        List<Contact> contacts = new ArrayList<>();
+        List<Contact> expectedContacts = new ArrayList<>();
 
-        IntStream.range(0, 5).forEach(i -> contacts.add(generateContactWithAllFieldsFilled()));
+        IntStream.range(0, 5).forEach(contact -> {
+            Contact newContact = generateContactWithAllFieldsFilled();
 
-        contacts.forEach(contact -> contactRestClient.createContact(contact)
-            .then()
-            .statusCode(SC_CREATED));
+            Long contactId = contactRestClient.createContact(newContact)
+                .then()
+                .statusCode(SC_CREATED)
+                .extract()
+                .as(ContactPostResponseBody.class)
+                .getId();
+
+            newContact.setId(contactId);
+
+            expectedContacts.add(newContact);
+        });
 
         // Retrieve all contacts
-//        contactRestClient.getContacts()
+        List<Contact> actualContacts = contactRestClient.getContacts()
+            .then()
+            .statusCode(SC_OK)
+            .extract()
+            .as(ContactListGetResponseBody.class)
+            .getContent();
 
-        // Assert status code
+        assertThat(actualContacts).hasSameSizeAs(expectedContacts);
 
-        // Extract response body
-//        .extract().as(ContactListGetResponseBody.class);
+        actualContacts.sort(Comparator.comparing(Contact::getId));
+        expectedContacts.sort(Comparator.comparing(Contact::getId));
 
-        // Assert contact list (sort both lists before asserting)
-//        assertThat(responseBody).isEqualTo(contacts);
-
+        for (int i = 0; i < actualContacts.size(); i++) {
+            assertThat(actualContacts.get(i)).isEqualToComparingFieldByField(expectedContacts.get(i));
+        }
     }
 }
